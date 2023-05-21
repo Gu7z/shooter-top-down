d1d802612aace60b84a967eec07f3144196cb44f
Gu7z <gustavoferri13@gmail.com>
2023-05-21 02:57:24 -0400

    save

---
+++ Shoot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+ public GameObject bulletPrefab;   // The prefab of the bullet to be shot
+    public float shootForce = 10f;    // The force at which the bullet is shot
+    public KeyCode shootKey = KeyCode.Mouse0; // The key to shoot the bullet
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(shootKey))
+        {
+            ShootBullet();
+        }
+    }
+
+    private void ShootBullet()
+    {
+        // Get the mouse position in world coordinates
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Calculate the direction from the shooter to the mouse position
+        Vector2 direction = mousePosition - (Vector2)transform.position;
+        direction.Normalize();
+
+        // Instantiate the bullet prefab
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        // Apply force to the bullet in the specified direction
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.AddForce(direction * shootForce, ForceMode2D.Impulse);
+    }
+}
